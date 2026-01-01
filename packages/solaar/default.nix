0cb9d1d2e4516aa22c7b636a943fd85228fa6e8b
marcin <nixosgitemail.frivolous320@passmail.net>
2026-01-01 04:19:33 +0100

    new solaar config

---
+++ packages/solaar/default.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  fetchurl,
+  python3Packages,
+  gobject-introspection,
+  gtk3,
+  wrapGAppsHook3,
+  gdk-pixbuf,
+  libappindicator,
+  librsvg,
+  libnotify,
+}:
+python3Packages.buildPythonApplication rec {
+  pname = "solaar";
+  version = "1.1.19rc1";
+
+  src = fetchurl {
+    url = "https://github.com/pwr-Solaar/Solaar/archive/refs/tags/${version}.tar.gz";
+    hash = "sha256-Ucx6d+OwrZ/iy7tKUKTEJzY7tDTjBu83ydjGqJolYSE=";
+  };
+
+  # Budowanie modułów udev
+  outputs = ["out" "udev"];
+
+  nativeBuildInputs = [
+    gdk-pixbuf
+    gobject-introspection
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    libappindicator
+    librsvg
+    libnotify
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    evdev
+    gtk3
+    psutil
+    pygobject3
+    pyudev
+    pyyaml
+    xlib
+    dbus-python
+    typing-extensions
+  ];
+
+  postInstall = ''
+    ln -s $out/bin/solaar $out/bin/solaar-cli
+    install -Dm444 -t $udev/etc/udev/rules.d rules.d-uinput/*.rules
+  '';
+
+  # Wyłączamy testy, żeby przyspieszyć budowanie
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Linux device manager for Logitech devices";
+    homepage = "https://pwr-solaar.github.io/Solaar/";
+    license = licenses.gpl2Only;
+    platforms = platforms.linux;
+  };
+}
