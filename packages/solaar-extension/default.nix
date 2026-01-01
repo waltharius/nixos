e5bc8bc385207db80afacfa066db93b337f692e7
marcin <nixosgitemail.frivolous320@passmail.net>
2026-01-01 02:21:31 +0100

    fixing proper repository for solaar extension adn adding additional configuration

--- packages/solaar-extension/default.nix
+++ packages/solaar-extension/default.nix
@@ -5,21 +5,20 @@
 }:
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-solaar";
-  version = "4";
+  version = "2.1.3+5";
 
   src = fetchFromGitHub {
     owner = "sidevesh";
     repo = "solaar-extension-for-gnome";
-    rev = "56377ee7cc375b4260d7713483013000b213b185";
-    hash = "sha256-n5+C/fTfQz2t1TjT3eFv+s5D/K/lE/d/S/X/o/C/h/g=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-sueTC3YRyIiOrLNAi0G3bWfxn/ml4Bwzw1qlWpBZJys=";
   };
-
   uuid = "solaar-extension@sidevesh";
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/gnome-shell/extensions/${uuid}
-    cp -r * $out/share/gnome-shell/extensions/${uuid}
+    cp -r solaar-extension@sidevesh/* $out/share/gnome-shell/extensions/${uuid}/
     runHook postInstall
   '';
 
