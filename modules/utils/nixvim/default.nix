49be955018686f2e40b075ae38c3a94f27ee6e80
waltharius <13510128+waltharius@users.noreply.github.com>
2026-01-04 23:25:00 +0100

    fix: correct nixvim default.nix structure

--- modules/utils/nixvim/default.nix
+++ modules/utils/nixvim/default.nix
@@ -1,23 +1,23 @@
 # NixVim Configuration Module
 # Drop-in replacement for modules/utils/neovim.nix
-{...}: {
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./core.nix
+    ./plugins.nix
+    ./lsp.nix
+    ./completion.nix
+    ./keymaps.nix
+    ./formatting.nix
+    ./org-mode.nix
+  ];
+
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-
-    # Import all sub-configurations
-    imports = [
-      ./options.nix
-      ./core.nix
-      ./plugins.nix
-      ./lsp.nix
-      ./completion.nix
-      ./keymaps.nix
-      ./formatting.nix
-      ./org-mode.nix
-    ];
   };
 }
