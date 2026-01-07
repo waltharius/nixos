a6a2852e17c76d9babb7ced508ed435c6b2c3d07
marcin <nixosgitemail.frivolous320@passmail.net>
2026-01-07 21:08:55 +0100

    playing with tmuxh plugins

--- modules/home/terminal/tmux.nix
+++ modules/home/terminal/tmux.nix
@@ -1,40 +1,79 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     mouse = true;
     escapeTime = 0;
-    historyLimit = 1000000;
+    historyLimit = 5000000;
     baseIndex = 1;
     terminal = "tmux-256color";
 
     plugins = with pkgs.tmuxPlugins; [
       sensible
       yank
-      catppuccin
-      tmux-which-key
-      tmux-fzf
-      tmux-powerline
-      sidebar
-      copy-toolkit
-      harpoon
-      jump
+      vim-tmux-navigator
+
+      cpu
+      battery
       kanagawa
-      net-speed
-      power-theme
-      prefix-highlight
-      resurrect
+
+      # Session persistence - keep at the end
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
       {
         plugin = continuum;
         extraConfig = ''
           set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '5'
+          set -g @continuum-save-interval '60'
         '';
       }
     ];
+
+    extraConfig = ''
+      # True color support
+      set -ga terminal-overrides ",xterm-256color:Tc"
+      set -as terminal-features ",xterm-256color:RGB"
+      set -g focus-events on
+
+      # Status bar positioning
+      set -g status-position bottom
+      set -g status-interval 2
+
+      # Clipboard (Wayland)
+      set -g set-clipboard on
+      set -s copy-command 'wl-copy'
+
+      # Vi mode
+      setw -g mode-keys vi
+      bind-key -T copy-mode-vi 'v' send -X begin-selection
+      bind-key -T copy-mode-vi 'y' send -X copy-pipe-and-cancel
+
+      # Pane navigation
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # Pane resizing
+      bind -r H resize-pane -L 5
+      bind -r J resize-pane -D 5
+      bind -r K resize-pane -U 5
+      bind -r L resize-pane -R 5
+
+      # Split with current path
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+
+      # Reload config
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
+
+      # Renumber windows
+      set -g renumber-windows on
+    '';
   };
 }
