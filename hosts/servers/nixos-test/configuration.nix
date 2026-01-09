eeffd0db529049940391bb0e856da36ede8efff4
waltharius <13510128+waltharius@users.noreply.github.com>
2026-01-09 23:56:41 +0100

    Move nixos-test to hosts/servers and add nixadm user

---
+++ hosts/servers/nixos-test/configuration.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    "${modulesPath}/virtualisation/proxmox-lxc.nix"
+    ../../../modules/system/certificates.nix
+    ../../../modules/servers/users.nix  # Add nixadm user
+  ];
+
+  system.stateVersion = "25.11";
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  networking = {
+    hostName = "nixos-test";
+    domain = "home.lan";  # DNS integration with FreeIPA
+    search = [ "home.lan" ];
+    nameservers = [ "192.168.50.1" ];  # FreeIPA DNS
+    useDHCP = lib.mkDefault true;
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [22];
+  };
+
+  # Use systemd-resolved for DNS (with FreeIPA)
+  services.resolved = {
+    enable = true;
+    dnssec = "false";
+    domains = ["home.lan"];
+    fallbackDns = ["9.9.9.9"];  # Quad9 fallback
+    extraConfig = ''
+      [Resolve]
+      DNS=192.168.50.1
+      Domains=home.lan
+      DNSoverTLS=no
+    '';
+  };
+
+  time.timeZone = "Europe/Warsaw";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Common server packages
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    curl
+    htop
+    bind
+    eza       # Modern ls replacement
+    zoxide    # Smart cd
+    starship  # Customizable prompt
+  ];
+
+  # SSH configuration (managed by users.nix module)
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
+}
