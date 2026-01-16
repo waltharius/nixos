9e1f46c4fdfc6def666d190a15a47d1ad3ca2c5f
waltharius <13510128+waltharius@users.noreply.github.com>
2026-01-16 20:29:36 +0100

    feat: add GNOME base packages module

---
+++ modules/home/desktop/gnome/base.nix
@@ -0,0 +1,24 @@
+# modules/home/desktop/gnome/base.nix
+# Core GNOME packages and basic configuration
+
+{pkgs, ...}: {
+  # Core GNOME user packages
+  home.packages = with pkgs; [
+    gnome-tweaks
+    dconf-editor
+    nautilus
+    eog
+    file-roller
+    gnome-system-monitor
+    gnome-screenshot
+  ];
+
+  # GTK theme - you like light theme!
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
+}
