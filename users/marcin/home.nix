9fbb8914d73f53c7cdfc4115828f5319a6b22e13
marcin <nixosgitemail.frivolous320@passmail.net>
2026-01-21 00:55:13 +0100

    stick to foliate instead

--- users/marcin/home.nix
+++ users/marcin/home.nix
@@ -182,8 +182,7 @@ in {
       pkgs-unstable.vivaldi
       shotwell
       nextcloud-client
-      koreader
-      readest
+      foliate
 
       # Productivity & Office
       libreoffice-fresh # Office suite
