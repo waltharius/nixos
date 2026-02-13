c020c45d1708c1c69b1ab7f41041b4442bd27d1d
marcin <nixosgitemail.frivolous320@passmail.net>
2026-02-13 13:39:27 +0100

    change tlp behavior on AC

--- modules/laptop/tlp.nix
+++ modules/laptop/tlp.nix
@@ -12,10 +12,10 @@
 
     settings = {
       # CPU settings
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       CPU_MIN_PERF_ON_AC = 0;
@@ -28,7 +28,7 @@
       CPU_BOOST_ON_BAT = 0;
 
       # Platform profile (for modern laptops)
-      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_AC = "balanced";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
       # Battery charge thresholds (prevents battery degradation)
