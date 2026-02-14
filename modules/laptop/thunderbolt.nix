89b70df149e084fecad1896483991ad05e8eab02
marcin <nixosgitemail.frivolous320@passmail.net>
2026-02-14 08:29:47 +0100

    back to simple thunderbolt main module. Specialised module for thunderbolt on T16 Gen3 will be added to supplemt this one

--- modules/laptop/thunderbolt.nix
+++ modules/laptop/thunderbolt.nix
@@ -1,32 +1,15 @@
-# modules/laptop/thunderbolt.nix
-{pkgs, ...}: {
+# Thunderbolt 3 support for docking stations
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # Enable Thunderbolt support
   services.hardware.bolt.enable = true;
 
+  # Add thunderbolt management tool
   environment.systemPackages = with pkgs; [
-    bolt
+    bolt # Thunderbolt device management
   ];
-
-  # Add systemd service to reinitialize Thunderbolt after resume
-  systemd.services.thunderbolt-resume = {
-    description = "Reinitialize Thunderbolt after resume";
-    after = ["suspend.target" "hibernate.target" "hybrid-sleep.target"];
-    wantedBy = ["suspend.target" "hibernate.target" "hybrid-sleep.target"];
-
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.kmod}/bin/modprobe -r thunderbolt && ${pkgs.kmod}/bin/modprobe thunderbolt";
-    };
-  };
-
-  # Ensure Thunderbolt devices are authorized on wake
-  systemd.services.bolt-authorize-resume = {
-    description = "Authorize Thunderbolt devices after resume";
-    after = ["suspend.target" "hibernate.target"];
-    wantedBy = ["suspend.target" "hibernate.target"];
-
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.bolt}/bin/boltctl list | ${pkgs.gnugrep}/bin/grep -v authorized | ${pkgs.gawk}/bin/awk \"{print $1}\" | ${pkgs.findutils}/bin/xargs -I {} ${pkgs.bolt}/bin/boltctl authorize {}'";
-    };
-  };
 }
