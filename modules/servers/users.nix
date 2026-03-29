7ba4ffc2b323b77a691bf2f440334906caaa5b7a
marcin <nixosgitemail.frivolous320@passmail.net>
2026-03-29 19:32:57 +0200

    add public key from juiceSSH for nixadm user

--- modules/servers/users.nix
+++ modules/servers/users.nix
@@ -1,8 +1,7 @@
 # Server user management
 # Defines nixadm user for server administration
 # Used on all servers instead of direct root login
-
-{ config, lib, pkgs, ... }: {
+{lib, ...}: {
   # Disable root SSH login for security
   services.openssh.settings = {
     PermitRootLogin = lib.mkForce "no";
@@ -12,11 +11,12 @@
   users.users.nixadm = {
     isNormalUser = true;
     description = "NixOS Administrator";
-    extraGroups = [ "wheel" ];  # Enable sudo
-    
+    extraGroups = ["wheel"]; # Enable sudo
+
     # SSH key authentication only
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhyNxm4pZR9CCnWGlDA+jotcnH5sc53LpSkSLs7XNx0 walth@fedora-laptop-tabby-2025"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNK9DIORkZzPOOya7WW3LpeaYYMCTtfC33/uz9fLupV JuiceSSH"
     ];
   };
 
