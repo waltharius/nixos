1d8341b9068741ce31bc93759ebc0d7e180480b8
waltharius <13510128+waltharius@users.noreply.github.com>
2026-06-02 20:51:47 +0200

    fix: adopt yazi shellWrapperName = "y" default for NixOS 26.05

--- modules/utils/yazi.nix
+++ modules/utils/yazi.nix
@@ -1,62 +1,68 @@
-{pkgs, ...}: {
+# modules/utils/yazi.nix
+#
+# Yazi terminal file manager.
+#
+# programs.yazi.shellWrapperName: in NixOS 26.05 the default shell wrapper
+# was renamed from "yy" to "y" (a shorter, less conflicting name). The
+# value is set explicitly here so that the behaviour is clear and no
+# evaluation warning is emitted regardless of home.stateVersion.
+{ pkgs, ... }: {
   programs.yazi = {
     enable = true;
-    enableBashIntegration = true; # Auto-cd functionality
+    enableBashIntegration = true;
     package = pkgs.yazi;
+    shellWrapperName = "y";
 
     settings = {
       mgr = {
-        ratio = [1 3 4];
+        ratio = [ 1 3 4 ];
         sort_by = "natural";
         sort_dir_first = true;
         show_hidden = true;
       };
 
-      # Configure Vim as the editor
       opener = {
         edit = [
           {
             run = ''vim "$@"'';
-            block = true; # Open in terminal
+            block = true;
             desc = "Edit";
             for = "unix";
           }
         ];
       };
 
-      # Force text files to use the 'edit' opener defined above
       open = {
         rules = [
           {
             name = "*/";
-            use = ["edit" "open" "reveal"];
+            use = [ "edit" "open" "reveal" ];
           }
           {
             mime = "text/*";
-            use = ["edit" "reveal"];
+            use = [ "edit" "reveal" ];
           }
           {
             mime = "image/*";
-            use = ["open" "reveal"];
+            use = [ "open" "reveal" ];
           }
           {
             mime = "{audio,video}/*";
-            use = ["play" "reveal"];
+            use = [ "play" "reveal" ];
           }
           {
             mime = "inode/x-empty";
-            use = ["edit" "reveal"];
+            use = [ "edit" "reveal" ];
           }
           {
             mime = "application/json";
-            use = ["edit" "reveal"];
+            use = [ "edit" "reveal" ];
           }
         ];
       };
     };
   };
 
-  # Ensure dependencies are available
   home.packages = with pkgs; [
     file
     ffmpeg
