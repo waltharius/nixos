1a82ba70f30e71b85c2428b9f3ea62ca54fe3316
marcin <nixosgitemail.frivolous320@passmail.net>
2026-06-30 15:32:33 +0200

    change how dictionary are configured

--- users/marcin/base/packages.nix
+++ users/marcin/base/packages.nix
@@ -11,6 +11,8 @@
   pkgs,
   pkgs-unstable,
   customPkgs,
+  config,
+  lib,
   ...
 }: {
   home.packages = with pkgs; [
@@ -114,9 +116,35 @@
     liberation_ttf
 
     # --- language / spell checking ---
-    hunspell
-    hunspellDicts.en_GB-large
-    hunspellDicts.pl_PL
+    # hunspell with UTF-8 capable dictionaries.
+    # pl_PL from Nixpkgs is always ISO8859-2; we provide a converted UTF-8
+    # copy via home.activation (see below). en_GB-large is already UTF-8.
+    (hunspell.withDicts (dicts: with dicts; [en_GB-large]))
     languagetool
   ];
+
+  # ---------------------------------------------------------------------------
+  # Hunspell UTF-8 dictionary for pl_PL
+  #
+  # hunspellDicts.pl_PL ships ISO8859-2 which causes iconv errors at runtime.
+  # We convert the .aff and .dic files to UTF-8 once, storing them in
+  # ~/.local/share/hunspell/.  Emacs points DICPATH there (see 03-spelling.el).
+  # The activation re-runs only when the source file is newer than the output.
+  # ---------------------------------------------------------------------------
+  home.activation.hunspellUtf8 = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    SRC="/etc/profiles/per-user/${config.home.username}/share/hunspell"
+    DST="$HOME/.local/share/hunspell"
+    $DRY_RUN_CMD mkdir -p "$DST"
+
+    for lang in pl_PL; do
+      if [ "$SRC/''${lang}.aff" -nt "$DST/''${lang}.aff" ] || [ ! -f "$DST/''${lang}.aff" ]; then
+        $DRY_RUN_CMD ${pkgs.glibc.bin}/bin/iconv -f ISO8859-2 -t UTF-8 \
+          "$SRC/''${lang}.aff" \
+          | ${pkgs.gnused}/bin/sed 's/^SET ISO8859-2/SET UTF-8/' \
+          > "$DST/''${lang}.aff"
+        $DRY_RUN_CMD ${pkgs.glibc.bin}/bin/iconv -f ISO8859-2 -t UTF-8 \
+          "$SRC/''${lang}.dic" > "$DST/''${lang}.dic"
+      fi
+    done
+  '';
 }
